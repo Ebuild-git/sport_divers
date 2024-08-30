a7827a05da9f7931c774de42382f1a96a91b144a
TURBOSOFT <achref.hamouda@e-build.tn>
2024-08-30 13:46:03 +0200

    update contact

--- resources/views/admin/contacts/Modal-message.blade.php
+++ resources/views/admin/contacts/Modal-message.blade.php
@@ -4,15 +4,38 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h6 class="modal-title" id="myCenterModalLabel">
-                    <i class='bx bxs-message-rounded-dots' ></i>{{ $cat->nom }}
+              Nom:      <i class='bx bxs-message-rounded-dots' ></i>{{ $cat->nom }}
                 </h6>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <p>
-                    {{ $cat->message ?? '' }}
+             Sexe:       {{ $cat->gender ?? '' }}
                 </p>
             </div>
+            <div class="modal-body">
+                <p>
+             CNI:       {{ $cat->cni ?? '' }}
+                </p>
+            </div>
+            <div class="modal-body">
+                <p>
+              Téléphone :    {{ $cat->telephone ?? '' }}
+                </p>
+            </div>
+            <div class="modal-body">
+                <p>
+              Age :    {{ $cat->age ?? '' }}
+                </p>
+            </div>
+            <div class="modal-body">
+                <p>
+            Message:        {{ $cat->message ?? '' }}
+                </p>
+            </div>
+
+
+
         </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
