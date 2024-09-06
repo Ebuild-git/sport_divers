f6686ba1bbbccf4d953eecb4021b19c464f60580
TURBOSOFT <achref.hamouda@e-build.tn>
2024-09-06 12:33:03 +0200

    update

---
+++ database/migrations/2024_09_06_103146_add_column_to_configs.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('configs', function (Blueprint $table) {
+            $table->integer("cin")->nullable();
+            $table->date("birthdate")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('configs', function (Blueprint $table) {
+            $table->dropColumn("cin");
+            $table->dropColumn("birthdate");
+        });
+    }
+};
