c12ff875a4d00f99ee23603ec36289e2ae0de6fd
kg <48618359+kevingassam@users.noreply.github.com>
2024-09-12 19:43:24 -0400

    fixer le style du formualaire de contact

--- resources/views/livewire/front/contact-form.blade.php
+++ resources/views/livewire/front/contact-form.blade.php
@@ -14,44 +14,45 @@
     <form id="contact-form" class="contact-form" wire:submit="save">
 
         <div class="row">
-         
+
             <div class="col-lg-6">
                 <div class="from-control">
                     <input wire:model="nom" type="text" placeholder="Votre nom" id="name" required="required">
                     @error('nom')
-                    <span class="small text-danger">
-                        {{ $message }}
-                    </span>
-                @enderror
+                        <span class="small text-danger">
+                            {{ $message }}
+                        </span>
+                    @enderror
                 </div>
             </div>
-               
-           
+
+
 
             <div class="col-lg-6">
                 <div class="from-control">
-                    <input  wire:model="email" type="email" placeholder="E-Mail" id="email" required="required">
+                    <input wire:model="email" type="email" placeholder="E-Mail" id="email" required="required">
                     @error('email')
-                    <span class="small text-danger">
-                        {{ $message }}
-                    </span>
-                @enderror
+                        <span class="small text-danger">
+                            {{ $message }}
+                        </span>
+                    @enderror
                 </div>
             </div>
             <div class="col-lg-6">
                 <div class="from-control">
-                    <input wire:model="telephone" type="number" placeholder="Votre téléphone" id="telephone" required="required">
+                    <input wire:model="telephone" type="number" placeholder="Votre téléphone" id="telephone"
+                        required="required">
                     @error('telephone')
-                    <span class="small text-danger">
-                        {{ $message }}
-                    </span>
-                @enderror
+                        <span class="small text-danger">
+                            {{ $message }}
+                        </span>
+                    @enderror
                 </div>
             </div>
 
             <div class="col-lg-6">
                 <div class="from-control">
-                    <input wire:model="birthdate" type="date" placeholder="Age" id="birthdate" required="required" >
+                    <input wire:model="birthdate" type="date" placeholder="Age" id="birthdate" required="required">
                     @error('birthdate')
                         <span class="small text-danger">
                             {{ $message }}
@@ -62,11 +63,16 @@
 
             <div class="col-lg-6">
                 <div class="form-control col-lg-12">
-                    <select wire:model="gender" id="sexe" class="  input-field w-100" >
-                        <option value="">Sélectionnez votre sexe</option>
-                        <option value="MALE">Homme</option>
-                        <option value="FEMALE">Femme</option>
-                    </select>
+                    <div class="row no-border">
+                        <div class="col my-auto ">
+                            <input type="radio" id="Homme" name="gender" wire:model='gender' required> 
+                            <label for="Homme">Homme</label>
+                        </div>
+                        <div class="col my-auto ">
+                            <input type="radio" id="Femme" name="gender" wire:model='gender' required>
+                            <label for="Femme">Femme</label>
+                        </div>
+                    </div>
                     @error('gender')
                         <span class="small text-danger error-message">
                             {{ $message }}
@@ -77,15 +83,8 @@
             <br><br>
             <div class="col-lg-6">
                 <div class="form-control col-lg-12 p-1">
-                    <input 
-                        wire:model="cin"  
-                        type="text" 
-                        placeholder="CIN" 
-                        id="cin" 
-                        class="input-field w-100" 
-                        required 
-                        maxlength="8" 
-                        pattern="[01][0-9]{7}" 
+                    <input wire:model="cin" type="text" placeholder="CIN" id="cin"
+                        class="input-field no-border w-100" required maxlength="8" pattern="[01][0-9]{7}"
                         title="CIN doit avoir 8 chiffres, et le premier chiffre doit être 0 ou 1"
                         oninput="this.value = this.value.slice(0, 8);">
                     @error('cin')
@@ -95,26 +94,9 @@
                     @enderror
                 </div>
             </div>
-            
-            
-            
-          {{-- 
-            <div class="col-lg-6">
-                <div class="form-control col-lg-12 p-1">
-                    <input wire:model="cni"  type="number" placeholder="CIN" id="cni" class="input-field w-100" required minlength="8" maxlength="8" pattern="[01]{2}[0-9]{6}" title="CIN doit avoir 8 chiffres et les deux premier sont composés entre 0 et 1">
-                    @error('cni')
-                        <span class="small text-danger error-message">
-                            {{ $message }}
-                        </span>
-                    @enderror
-                </div>
-            </div> --}}
-
-           
-
-            <div class="col-lg-12">
+            <div class="col-lg-12 mt-2">
                 <div class="from-control">
-                    <input wire:model="sujet" type="text" placeholder="Sujet" id="subject"  required="required">
+                    <input wire:model="sujet" type="text" placeholder="Sujet" id="subject" required="required">
                     @error('sujet')
                         <span class="small text-danger">
                             {{ $message }}
@@ -123,25 +105,34 @@
                 </div>
             </div>
 
-
         </div>
-            <div class="from-control">
-                <textarea wire:model="message"  placeholder="Laissez votre message"  id="message" required="required"></textarea>
-                @error('message')
+        <div class="from-control">
+            <textarea wire:model="message" placeholder="Laissez votre message" id="message" required="required"></textarea>
+            @error('message')
                 <span class="small text-danger">
                     {{ $message }}
                 </span>
             @enderror
-            </div>
+        </div>
 
-            <div class="submit-btn">
-                <button class="readon" type="submit">
-                    <span wire:loading>
-                        <img src="/icons/kOnzy.gif" height="20" width="20" alt="" srcset="">
-                    </span>
-                    Envoyez le Message
-                </button>
-            </div>
+        <div class="submit-btn">
+            <button class="readon" type="submit">
+                <span wire:loading>
+                    <img src="/icons/kOnzy.gif" height="20" width="20" alt="" srcset="">
+                </span>
+                Envoyez le Message
+            </button>
+        </div>
     </form>
 
+
+    <style>
+        .no-border {
+            border: solid 1px white !important;
+            box-shadow: none !important;
+            height: 38px;
+            width: 100%;
+            outline-style: none !important;
+        }
+    </style>
 </div>
