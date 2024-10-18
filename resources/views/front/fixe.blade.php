4156a3cf3a2e534153df0724f4d949631967035f
TURBOSOFT <achref.hamouda@e-build.tn>
2024-10-18 18:14:01 +0200

    update

--- resources/views/front/fixe.blade.php
+++ resources/views/front/fixe.blade.php
@@ -402,12 +402,14 @@
         <script src="js/jquery.counterup.min.js"></script>
         <script src="js/waypoints.min.js"></script>
         
-        <script src="js/jquery.nice-select.min.js"></script>
+       
         <script src="js/swiper.min.js"></script>
        
         <script src="js/main.js"></script>
 
-        <script src="/assets/js/jquery.nice-select.min.js"></script>
+        {{-- <script src="/assets/js/jquery.nice-select.min.js"></script> --}}
+        <script src="/js/jquery.nice-select.min.js"></script>
+
 
 </body>
 
